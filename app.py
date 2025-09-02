38f1e24ea6ece2a649b54ca2b8af43a3e5630090
bsetec <bsetec@desktop20bsetec.com>
2025-09-02 12:27:20 +0530

    container stoped and run

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Docker + Jenkins Pipeline!"
+    return "Hello from Dockers + Jenkins Pipeline!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
